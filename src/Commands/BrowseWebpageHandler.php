4ed1661081929dfe25247062ad433ad86ef9bc39
Frank van den Brink <frank@pragmatist.nl>
2023-04-16 04:53:46 +0200

    Refactored BrowseWebpage to summarize pages in chunks to avoid token limit

--- src/Commands/BrowseWebpageHandler.php
+++ src/Commands/BrowseWebpageHandler.php
@@ -11,6 +11,8 @@ use Pragmatist\Assistant\Commands\BrowseWebpage\TextExtractor;
 
 final class BrowseWebpageHandler implements CommandHandler
 {
+    const MAX_CHUNK_SIZE = 8192;
+
     public function __construct(
         private TextExtractor $textExtractor,
         private HttpClient $httpClient,
@@ -38,7 +40,55 @@ final class BrowseWebpageHandler implements CommandHandler
         );
     }
 
+    private function fetchPage(string $url): string
+    {
+        return (string) $this->httpClient->get($url)->getBody();
+    }
+
     private function analyzeText(string $text, string $question): string
+    {
+        $chunks = $this->divideTextIntoChunks($text);
+        $chunkSummaries = [];
+
+        foreach ($chunks as $chunk) {
+            $chunkSummaries[] = $this->summarizeText($chunk, $question);
+        }
+
+        $combinedSummary = implode("\n", $chunkSummaries);
+
+        return $this->summarizeText($combinedSummary, $question);
+    }
+
+    private function divideTextIntoChunks(string $text): array
+    {
+        $chunks = [];
+        $currentChunk = '';
+        $paragraphs = $this->splitTextIntoParagraphs($text);
+
+        foreach ($paragraphs as $paragraph) {
+            $tempChunk = $currentChunk . "\n" . $paragraph;
+
+            if (strlen($tempChunk) <= self::MAX_CHUNK_SIZE) {
+                $currentChunk = $tempChunk;
+            } else {
+                $chunks[] = trim($currentChunk);
+                $currentChunk = $paragraph;
+            }
+        }
+
+        if (!empty($currentChunk)) {
+            $chunks[] = trim($currentChunk);
+        }
+
+        return $chunks;
+    }
+
+    private function splitTextIntoParagraphs(string $text): array
+    {
+        return preg_split('/\n/', $text, -1, PREG_SPLIT_NO_EMPTY);
+    }
+
+    private function summarizeText(string $text, string $question): string
     {
         $response = $this->openAIClient->chat()->create(
             [
@@ -48,16 +98,11 @@ final class BrowseWebpageHandler implements CommandHandler
                     ['role' => 'user', 'content' => '"""' . $text . '"""'],
                     ['role' => 'user', 'content' => 'Using the above text, please answer the following question: "' . $question . '"'],
                     ['role' => 'user', 'content' => 'Do not provide other information beyond answering the question.'],
-                    ['role' => 'user', 'content' => 'If the question cannot be answered, explicitly say so and then summarize the text.']
+                    ['role' => 'user', 'content' => 'Summarize the text if the question cannot be answered directly.']
                 ]
             ]
         );
 
         return $response->choices[0]->message->content;
-    }
-
-    private function fetchPage(string $url): string
-    {
-        return (string) $this->httpClient->get($url)->getBody();
     }
 }
