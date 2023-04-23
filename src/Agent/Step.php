ab8b79ceba73ba2058662bdf6ba6d72260799983
Frank van den Brink <frank@pragmatist.nl>
2023-04-23 12:47:22 +0200

    Added step class for agent steps

---
+++ src/Agent/Step.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vexo\Weave\Agent;
+
+final class Step
+{
+    public function __construct(
+        private Action|Finish $action,
+        private string $log,
+        private ?string $observation = null
+    ) {
+    }
+
+    public function withObservation(string $observation): self
+    {
+        return new Step(
+            $this->action(),
+            $this->log(),
+            $observation
+        );
+    }
+
+    public function action(): Action|Finish
+    {
+        return $this->action;
+    }
+
+    public function log(): string
+    {
+        return $this->log;
+    }
+
+    public function observation(): ?string
+    {
+        return $this->observation;
+    }
+}
---
+++ src/Agent/StepTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Vexo\Weave\Agent;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(Step::class)]
+final class StepTest extends TestCase
+{
+    public function testConstructorAndGetters(): void
+    {
+        $action = new Action('google', 'Best restaurants in Amsterdam');
+        $step = new Step($action, 'Some log');
+
+        $this->assertSame($action, $step->action());
+        $this->assertEquals('Some log', $step->log());
+    }
+
+    public function testWithObservation(): void
+    {
+        $action = new Action('google', 'Best restaurants in Amsterdam');
+        $step = new Step($action, 'Some log');
+
+        $this->assertNull($step->observation());
+
+        $step = $step->withObservation('Some observation');
+        $this->assertEquals('Some observation', $step->observation());
+    }
+}
