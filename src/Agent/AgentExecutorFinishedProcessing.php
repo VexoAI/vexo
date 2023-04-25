8a702db5372224a35e67449f7c671415d1d3cad5
Frank van den Brink <frank@pragmatist.nl>
2023-04-25 21:04:10 +0200

    Removed unused attribute

--- src/Agent/AgentExecutorFinishedProcessing.php
+++ src/Agent/AgentExecutorFinishedProcessing.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace Vexo\Agent;
 
-use PHPUnit\Framework\Attributes\CodeCoverageIgnore;
 use Vexo\SomethingHappened;
 
-#[CodeCoverageIgnore]
 final class AgentExecutorFinishedProcessing extends SomethingHappened
 {
     public function __construct(
