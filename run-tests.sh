a30979cbc42f708513fe68ec9d579f6ed6bb9462
Frank van den Brink <frank@pragmatist.nl>
2023-04-30 22:05:53 +0200

    Added --diff to php-cs-fixer

--- run-tests.sh
+++ run-tests.sh
@@ -2,4 +2,4 @@
 
 vendor/bin/phpunit --coverage-text --coverage-html tmp/coverage && \
 vendor/bin/phpstan analyse && \
-vendor/bin/php-cs-fixer fix --cache-file=tmp/.php-cs-fixer.cache --allow-risky=yes --dry-run
+vendor/bin/php-cs-fixer fix --cache-file=tmp/.php-cs-fixer.cache --allow-risky=yes --dry-run --diff
