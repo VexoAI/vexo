5eb814c321f2895d9d72f5fa3daec41c69b69482
Frank van den Brink <frank@pragmatist.nl>
2023-05-01 20:58:15 +0200

    Auto clear rector cache

--- run-tests.sh
+++ run-tests.sh
@@ -3,4 +3,4 @@
 vendor/bin/phpunit --coverage-text --coverage-html tmp/coverage && \
 vendor/bin/phpstan analyse && \
 vendor/bin/php-cs-fixer fix --cache-file=tmp/.php-cs-fixer.cache --allow-risky=yes --dry-run --verbose --diff &&
-vendor/bin/rector process --dry-run
+vendor/bin/rector process --clear-cache --dry-run
