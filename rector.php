824dc698ea88016194bcc1048fe31ff2418cd46b
Frank van den Brink <frank@pragmatist.nl>
2023-05-06 09:28:08 +0200

    Include examples in rector

--- rector.php
+++ rector.php
@@ -13,6 +13,7 @@ use Rector\Set\ValueObject\SetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
         __DIR__ . '/src',
+        __DIR__ . '/examples'
     ]);
 
     $rectorConfig->rules([
