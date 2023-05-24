e1d59c320280462b43aad3d5fb0cff9a8f018b4f
Frank van den Brink <frank@pragmatist.nl>
2023-05-24 12:56:38 +0200

    Expanded run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-vendor/bin/phpunit --coverage-text --coverage-html tmp/coverage && \
-vendor/bin/phpstan analyse && \
-vendor/bin/php-cs-fixer fix --cache-file=tmp/.php-cs-fixer.cache --allow-risky=yes --dry-run --verbose --diff &&
-vendor/bin/rector process --dry-run
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BIN_DIR="$SCRIPT_DIR/vendor/bin"
+TMP_DIR="$SCRIPT_DIR/tmp"
+
+phpcs_options="--dry-run"
+rector_options="--dry-run"
+
+while (( "$#" )); do
+  case "$1" in
+    --fix-cs)
+      phpcs_options=""
+      shift
+      ;;
+    --fix-rector)
+      rector_options=""
+      shift
+      ;;
+    *)
+      echo "Error: Invalid argument"
+      exit 1
+      ;;
+  esac
+done
+
+$BIN_DIR/phpunit --coverage-text --coverage-html $TMP_DIR/coverage && \
+$BIN_DIR/phpstan analyse && \
+$BIN_DIR/php-cs-fixer fix --cache-file=$TMP_DIR/.php-cs-fixer.cache --allow-risky=yes --verbose --diff $phpcs_options &&
+$BIN_DIR/rector process $rector_options
